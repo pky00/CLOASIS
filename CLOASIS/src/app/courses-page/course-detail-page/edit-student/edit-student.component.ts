f4d012a0b2cb2c1cfb9bc76108bf5bc7659a55ee
Peter Yamout <pky00@mail.aub.edu>
2020-03-24 11:52:19 +0200

    Add page functionality added and Edit Page was also added with functionality

---
+++ CLOASIS/src/app/courses-page/course-detail-page/edit-student/edit-student.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/models/course.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CourseService } from 'src/app/services/course.service';
+import { Student } from 'src/app/models/student.model';
+
+@Component({
+  selector: 'app-edit-student',
+  templateUrl: './edit-student.component.html',
+  styleUrls: ['./edit-student.component.css']
+})
+export class EditStudentComponent implements OnInit {
+
+  course: Course;
+  addForm: FormGroup;
+  student: Student;
+
+  onSubmit(){
+    if(this.addForm.valid){
+      this.courseService.editStudent(this.student.id,{id:this.student.id,name: this.addForm.get('name').value,studentid: this.addForm.get('id').value,email: this.addForm.get('email').value,imagePath: ""});
+      this.router.navigate(['/COURSEDETAILPAGE/' + this.course.coursecode]);
+    }
+  }
+
+  constructor(private route: ActivatedRoute,private courseService: CourseService,private router:Router) { }
+
+  ngOnInit() {
+    this.course = this.courseService.currentCourse;
+    this.student = this.courseService.currentStudent;
+
+      this.addForm = new FormGroup({
+        'name' : new FormControl(this.student.name,[Validators.required]),
+        'id' : new FormControl(this.student.studentid,[Validators.required]),
+        'email' : new FormControl(this.student.email,[Validators.required,Validators.email])
+      });
+  }
+
+}
